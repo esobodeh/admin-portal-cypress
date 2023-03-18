2e67f1fade1379f4eda40090f6b280b332d4bfb7
Samuel Obodeh <samuelobodeh@Samuels-MacBook-Pro.local>
2023-03-18 09:24:07 +0100

    CA-13 added aftereach to delete created user

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,21 +1,13 @@
 
+import 'cypress-wait-until';
 // -- This is a parent command --
 Cypress.Commands.add('login', (email, password) => { 
-
+    cy.fixture('CloheaDummyData').then((data) => {
+        cy.visit('/login');
+        // cy.getElementByID(data.Login.usernameTextfield)
+        // .type()
+    })
  })
  Cypress.Commands.add('getElementByID', (selector) => { 
-    cy.get(`[data-test="${selector}"]`);
+    cy.get(`[data-testid="${selector}"]`);
 })
- 
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
--- cypress/tests/Onboarding/register.cy.js
+++ cypress/tests/Onboarding/register.cy.js
@@ -2,14 +2,53 @@
 
 context('Onboarding', () => {
     beforeEach(() => {
+      cy.fixture('selectors').as('selector')
+      cy.fixture('CloheaDummyData').as('testData')
       cy.visit('/')
     })
   
-    it('Verify user inputs valid email ', () => {
-      cy.get('p > a').click()
-      cy.getElementByID('register-emailInput')
-        .type('fake@email.com').should('have.value', 'fake@email.com')
+    it('Verify Registration with Valid User data', function () {
+      // -- Click on Sign Up Link/Button)
+      cy.getElementByID(this.selector.Login.signUpLinkedText).click()
+
+      // -- type RC number into RC Number field and assert that the right company name comes out
+      cy.getElementByID(this.selector.Onboarding.rcNumber)
+        .type(this.testData.Onboarding.rcNumber)
+      cy.getElementByID(this.selector.Onboarding.email).click()
+      cy.getElementByID(this.selector.Onboarding.mcCompanyName)
+        .should('have.value', this.testData.Onboarding.mcCompanyName)
+
+      // -- Enter email
+      cy.getElementByID(this.selector.Onboarding.email).click()
+        .type(this.testData.Onboarding.email)
+      
+      cy.get('input[placeholder*="Enter phone number"]')
+        .type(this.testData.Onboarding.phoneNumber)  
         
+        // -- Enter Password & Conform Password
+      cy.getElementByID(this.selector.Onboarding.password).click()
+        .type(this.testData.Onboarding.password)
+      cy.getElementByID(this.selector.Onboarding.confirmPassword).click()
+        .type(this.testData.Onboarding.password)
+
+      cy.get(':nth-child(1) > [data-v-7c5c21dc=""][data-v-59cbef9c=""] > span').click()
+
+      cy.getElementByID(this.selector.Onboarding.signUpButton).click()
+
+      cy.waitUntil(()=> cy.getElementByID(this.selector.Onboarding.confirmOTPBtn).should('have.text', ' Confirm Email '));
+      cy.wait(1000)
+      // cy.waitUntil(()=>  cy.getElementByID(this.selector.Onboarding.closeModalBtn).click())
+    })
+
+    afterEach(()=>{
+      cy.fixture('CloheaDummyData').then(function(data){
+        cy.request('DELETE', 'http://rootservice.development.clohea.com/api/rootservice/v1/super-admin/tenant',
+        { Email : data.Onboarding.email }).then(
+        (response) => {
+          expect(response.body).to.have.property('message', 'Tenant account deleted successfully') // true
+      })
+  }
+)
     })
   })
   
