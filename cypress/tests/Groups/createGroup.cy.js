cb94cf968cf469825f6cdbf028679e60774d400a
Michael Olarewaju netow <Olarewajulmnetow@sterling.ng>
2023-07-07 18:51:28 +0100

    updated create Group

--- cypress/tests/Groups/createGroup.cy.js
+++ cypress/tests/Groups/createGroup.cy.js
@@ -5,6 +5,11 @@ context('createGroup', () => {
       cy.visit('/')
       cy.url().should('eq', 'http://adminportal.development.clohea.com/login') 
     })
+ 
+        var _date = new Date()
+        _date = _date.toUTCString()
+        let groupName = 'Group - ' + _date;
+
     it('should be able to create group',  function() {
       cy.getElementByID(this.selectors.Login.emailTextfield).clear().type(this.michaelTestdata.login.username)
       cy.getElementByID(this.selectors.Login.passwordTextfield).clear().type(this.michaelTestdata.login.password)
@@ -14,7 +19,7 @@ context('createGroup', () => {
       cy.url().should('eq', 'http://adminportal.development.clohea.com/manage-members')
       cy.getElementByID(this.selectors.manageMembersMenu.createGroup).click()
        cy.getElementByID(this.selectors.manageMembersMenu.groupnameField).click()
-       cy.getElementByID(this.selectors.manageMembersMenu.groupnameField).clear().type(this.michaelTestdata.groupInfo.groupname)
+       cy.getElementByID(this.selectors.manageMembersMenu.groupnameField).clear().type(groupName.toString())
        cy.getElementByID(this.selectors.manageMembersMenu.patientReg).invoke('show').eq(0).click()
        cy.getElementByID(this.selectors.manageMembersMenu.appointmentsBttn).invoke('show').eq(1).click()
        cy.getElementByID(this.selectors.manageMembersMenu.treatmentsBttn).invoke('show').eq(2).click()
